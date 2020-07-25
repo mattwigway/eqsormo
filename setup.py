a03418b957d1715fe0acaa3499603cb37ac14ac2
Matthew Wigginton Conway <matt@indicatrix.org>
2020-07-25 10:30:22 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eqsormo',
+    version='0.1.1',
+    packages=find_packages(),
+    author='Matthew Wigginton Conway',
+    author_email='matt@indicatrix.org',
+    description='Equilibrium sorting models in Python',
+    url='https://github.com/mattwigway/eqsormo',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License'
+    ]
+)
