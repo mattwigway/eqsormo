93ed4c6c67d6194429240a2b330288120667bf7c
Matthew Wigginton Conway <matt@indicatrix.org>
2020-12-09 17:11:32 -0500

    add type to setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='eqsormo',
-    version='0.3.10',
+    version='0.3.10-pypi-2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     requirements=['statsmodels>=0.11', 'tqdm>=4.0', 'numba>=0.51', 'numpy>=1.0', 'pandas>=1.0', 'dill>=0.3', 'scipy>=1.5'],
     author='Matthew Wigginton Conway',
     author_email='matt@indicatrix.org',
     description='Equilibrium sorting models in Python',
+    long_description_type='text/markdown',
     long_description=long_description,
     url='https://github.com/mattwigway/eqsormo',
     classifiers=[
