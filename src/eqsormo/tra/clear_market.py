1f95162c7f8a62b54d349135e7f1283399e4db5b
Matthew Wigginton Conway <matt@indicatrix.org>
2020-12-29 23:06:48 -0500

    experimental new way of adjusting prices

--- src/eqsormo/tra/clear_market.py
+++ src/eqsormo/tra/clear_market.py
@@ -44,7 +44,7 @@ def clear_market_iter(
     step=1e-2,
     weights=None,
     fixed_price=0,
-    speed_control=0.25
+    speed_control=0.25,
 ):
     """
     Run one iteration of the market-clearing algorithm.
@@ -80,7 +80,7 @@ def clear_market_iter(
         price_income_params=price_income_params,
         budget_coef=budget_coef,
         max_rent_to_income=max_rent_to_income,
-        weights=weights
+        weights=weights,
     )
 
     if np.any(shares == 0):
@@ -93,207 +93,10 @@ def clear_market_iter(
         np.sum(shares), np.sum(supply)
     ), "shares and supply totals do not match"
 
-    excess_demand = shares - supply
-
-    # maxdiff = np.max(np.abs(shares - supply) / supply)
-    maxdiff = np.max(np.abs(excess_demand / supply))
-    LOG.info(f"Max unit diff: {maxdiff}")
-
-    # Use the approach defined in Tra (2007), page 108, eq. 7.7/7.7a, which is in turn from Anas (1982).
-    alt_price = price[choiceidx]
-
-    budget = np.zeros_like(alt_income)
-    if max_rent_to_income is None:
-        feasible_alts = np.full_like(alt_price, True)
-    else:
-        feasible_alts = alt_income * max_rent_to_income > alt_price
-
-    budget = np.full_like(alt_income, np.nan)
-    budget[feasible_alts] = price_income_transformation.apply(
-        alt_income[feasible_alts], alt_price[feasible_alts], *price_income_params
-    )
-    budget_step = np.full_like(alt_income, np.nan)
-
-    if max_rent_to_income is None:
-        feasible_alts_step = np.full_like(alt_price, True)
-    else:
-        feasible_alts_step = alt_income * max_rent_to_income > alt_price + step
-
-    budget_step[feasible_alts_step] = price_income_transformation.apply(
-        alt_income[feasible_alts_step],
-        alt_price[feasible_alts_step] + step,
-        *price_income_params,
-    )
-
-    deriv = compute_derivatives(
-        price,
-        alt_income,
-        choiceidx,
-        hhidx,
-        non_price_utilities,
-        budget,
-        budget_step,
-        step,
-        budget_coef,
-        shares,
-        max_rent_to_income,
-        feasible_alts,
-        feasible_alts_step,
-        weights,
-    )
-
-    if not np.all(deriv < 0):
-        raise ValueError("some derivatives of price are nonnegative")
-
-    LOG.info(f"Computed price derivatives:\n{pd.Series(deriv).describe()}")
-
-    # this is 7.7a from Tra's dissertation
-    orig_fixed_price = price[fixed_price]
-    price = price - (excess_demand / deriv) * speed_control
-
-    # fix one price from changing so the system is defined
-    price[fixed_price] = orig_fixed_price
-
-    return price, False  # not converged yet (or we don't know anyhow)
-
-
-# Numba does not help with this function
-def compute_derivatives(
-    price,
-    alt_income,
-    choiceidx,
-    hhidx,
-    non_price_utilities,
-    budget,
-    budget_step,
-    price_step,
-    budget_coef,
-    base_shares,
-    max_rent_to_income,
-    feasible_alts,
-    feasible_alts_step,
-    weights,
-):
-    deriv = np.zeros_like(price)
-
-    base_exp_utilities = np.exp(non_price_utilities + budget_coef * budget)
-    # exp utility of zero is out of choice set
-    base_exp_utilities[~feasible_alts] = 0
-
-    # memmap the base utilities so we can use copy-on-write
-    fh, util_file = tempfile.mkstemp(prefix="eqsormo_mmap_", suffix=".bin")
-    os.close(fh)
-    try:
-        mm = np.memmap(
-            util_file, dtype="float64", mode="w+", shape=base_exp_utilities.shape
-        )
-        mm[:] = base_exp_utilities[:]
-        mm.flush()
-        if not np.all(np.isfinite(base_exp_utilities)):
-            raise FloatingPointError(
-                f"Not all exp(utilities) are finite (scaling?)\n"
-                f"min non-price utility: {np.min(non_price_utilities)}\n"
-                f"max non-price utility: {np.max(non_price_utilities)}\n"
-                f"nans in non-price utility: {np.sum(np.isnan(non_price_utilities))}\n"
-                f"min budget: {np.min(budget[feasible_alts])}\n"
-                f"max budget: {np.max(budget[feasible_alts])}\n"
-                f"nans in budget: {np.sum(np.isnan(budget[feasible_alts]))}\n"
-            )
-        del base_exp_utilities
-
-        step_exp_utilities = np.exp(non_price_utilities + budget_coef * budget_step)
-        step_exp_utilities[~feasible_alts_step] = 0
-
-        if not np.all(np.isfinite(step_exp_utilities)):
-            raise FloatingPointError(
-                "Not all exp(step_utilities) are finite (scaling?)\n"
-                f"min non-price utility: {np.min(non_price_utilities)}\n"
-                f"max non-price utility: {np.max(non_price_utilities)}\n"
-                f"nans in non-price utility: {np.sum(np.isnan(non_price_utilities))}\n"
-                f"min budget: {np.min(budget_step[feasible_alts_step])}\n"
-                f"max budget: {np.max(budget_step[feasible_alts_step])}\n"
-                f"nans in budget: {np.sum(np.isnan(budget_step[feasible_alts_step]))}\n"
-            )
-
-        # cache weights
-        if weights is not None:
-            alt_weights = weights[hhidx]
-
-        task_queue = queue.Queue()
-        result_queue = queue.Queue()
-        stop_threads = threading.Event()
-
-        def worker():
-            while not stop_threads.is_set():
-                try:
-                    i = task_queue.get(timeout=10)
-                except queue.Empty:
-                    continue  # return to top of loop to check for stop_threads event
-                else:
-                    exp_utilities = np.memmap(
-                        util_file,
-                        dtype="float64",
-                        mode="c",
-                        shape=step_exp_utilities.shape,
-                    )
-                    # save memory by just saving indices, not full bool array
-                    # in the model in my dissertation, there are ~1000 choices, so this will be 1/1000 of the indices
-                    # since an int is 32 bytes (or maybe 64 since numpy arrays can be big), and a bool I believe takes up
-                    # an entire byte, we come out ahead b/c 1/1000 * 32 < 8
-                    choicemask = np.nonzero(choiceidx == i)
-                    exp_utilities[choicemask] = step_exp_utilities[choicemask]
-                    util_sums = np.bincount(hhidx, weights=exp_utilities)
-                    probs = exp_utilities[choicemask] / util_sums[hhidx[choicemask]]
-                    if weights is not None:
-                        probs *= alt_weights[choicemask]
-                    share_step = np.sum(probs)
-                    del probs
-                    deriv = (share_step - base_shares[i]) / price_step
-                    result_queue.put((i, deriv))
-                    task_queue.task_done()
-
-        def consumer():
-            while not stop_threads.is_set():
-                try:
-                    i, val = result_queue.get(timeout=10)
-                except queue.Empty:
-                    continue
-                else:
-                    assert (
-                        val < 0
-                    ), f"derivative of price for alt {i} is non-negative: {val}"
-                    if i % 10 == 9:
-                        LOG.info(f"computed derivative {i + 1} / {len(deriv)}")
-                    deriv[i] = val
-                    result_queue.task_done()
-
-        # might need something more complex here to account for the memory pressure of sorting. Even if you have
-        # 16 cores, you might not have enough memory to compute 16 derivatives at once.
-        nthreads = multiprocessing.cpu_count()
-        LOG.info(f"computing derivatives using {nthreads} threads")
-
-        # start threads
-        for i in range(nthreads):
-            threading.Thread(target=worker, daemon=False).start()
-
-        # consumer thread
-        threading.Thread(target=consumer, daemon=False).start()
-
-        # fill queue
-        for i in range(len(price)):
-            task_queue.put(i)
-
-        # await completion
-        task_queue.join()
-        result_queue.join()
-
-        # signal threads to shut down
-        stop_threads.set()
-
-        return deriv
-    finally:
-        # clean up
-        os.remove(util_file)
+    # same logic as the ASC-finding algorithm, move prices that are too high lower and vice-versa
+    # it's possible however that since prices are inside a log and multiplied by a coef that there
+    # are some situations this could not climb out of.
+    return price - np.log(shares / supply), False
 
 
 def compute_shares(
